fix(kalman): fix dt calculation

neonfc_ssl/algorithms/kalman_filter/kalman_filter.py
@@ -12,7 +12,7 @@ class KalmanFilter:
         self._Ct = np.transpose(self._C)
 
         self._R = .1 * np.identity(var_size)
-        self._Q = .003 * np.identity(obs_size)
+        self._Q = .1 * np.identity(obs_size)
 
         self.belief = np.zeros([var_size, 1])
         self.uncert = np.zeros([var_size, var_size])

neonfc_ssl/entities/ball.py
@@ -40,8 +40,9 @@ class Ball(object):
         self._np_array = None
 
     def _update_kalman(self, create=False):
-        self.dt = self.lt - time.time()
-        self.lt = time.time()
+        t = time.time()
+        self.dt = t - self.lt
+        self.lt = t
 
         A = np.array([
             [1, 0, self.dt, 0],

neonfc_ssl/entities/robot.py
@@ -60,8 +60,9 @@ class OmniRobot:
         return robot_data
 
     def _update_kalman(self, create=False):
-        self.dt = self.lt - time.time()
-        self.lt = time.time()
+        t = time.time()
+        self.dt = t - self.lt
+        self.lt = t
 
         A = np.array([
             [1, 0, 0, self.dt, 0, 0],