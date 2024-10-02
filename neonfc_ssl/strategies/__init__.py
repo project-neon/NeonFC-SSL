adde3af82c3aec62cb7afce3ab4dc2092c29f9b1
gTorrez <g.ctorres09@gmail.com>
2024-10-02 13:01:17 -0300

    add(keeper): libero strategy in init

--- neonfc_ssl/strategies/__init__.py
+++ neonfc_ssl/strategies/__init__.py
@@ -2,3 +2,5 @@ from neonfc_ssl.strategies.still import Still
 from neonfc_ssl.strategies.receiver import Receiver
 from neonfc_ssl.strategies.ball_holder import BallHolder
 from neonfc_ssl.strategies.goalkeeper import GoalKeeper
+from neonfc_ssl.strategies.libero import Libero
+
