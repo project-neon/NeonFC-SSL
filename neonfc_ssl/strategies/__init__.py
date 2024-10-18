08a42ae5c8f75853addcce196cd229771892ae77
Joao Pedro Leal <jpkleal@gmail.com>
2024-10-18 20:57:40 -0300

    feat(real-life): create a simple one robot strategy and coach

--- neonfc_ssl/strategies/__init__.py
+++ neonfc_ssl/strategies/__init__.py
@@ -4,3 +4,4 @@ from neonfc_ssl.strategies.ball_holder import BallHolder
 from neonfc_ssl.strategies.goalkeeper import GoalKeeper
 from neonfc_ssl.strategies.libero import Libero
 from neonfc_ssl.strategies.base_strategy import BaseStrategy
+from neonfc_ssl.strategies.test import Test
