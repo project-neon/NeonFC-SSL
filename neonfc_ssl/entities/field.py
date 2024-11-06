ed28d2548e28216bd56d951b20f75f94793b9830
gTorrez <g.ctorres09@gmail.com>
2024-11-06 14:57:34 -0300

    fix(field): area depth and width in grsim

--- neonfc_ssl/entities/field.py
+++ neonfc_ssl/entities/field.py
@@ -5,6 +5,8 @@ from dataclasses import dataclass
 class Field:
     fieldLength: int = 9
     fieldWidth: int = 6
+    penaltyAreaDepth: int = 0
+    penaltyAreaWidth: int = 0
     goalWidth: int = 0
     leftGoalLine: tuple[int, int] = tuple((0, 0))
     rightGoalLine: tuple[int, int] = tuple((0, 0))
@@ -23,7 +25,8 @@ class Field:
         self.fieldLength = frame.get('fieldLength')
         self.fieldWidth = frame.get('fieldWidth')
         self.goalWidth = frame.get('goalWidth')
-
+        self.penaltyAreaDepth = frame.get('penaltyAreaDepth')  # nao tem no grsim, usar leftpenaltyscretch
+        self.penaltyAreaWidth = frame.get('penaltyAreaWidth')  # idem
         self.leftGoalLine = (
             frame.get('fieldLines').get('LeftGoalLine').get('p1').get('x') + .5 * self.fieldLength,
             frame.get('fieldLines').get('LeftGoalLine').get('p1').get('y') + .5 * self.fieldWidth
@@ -57,6 +60,16 @@ class Field:
         self.leftFirstPost = (
             frame.get('fieldLines').get('LeftGoalBottomLine').get('p1').get('x') + .5 * self.fieldLength,
             frame.get('fieldLines').get('LeftGoalBottomLine').get('p1').get('y') + .5 * self.fieldWidth
+        )
+
+        self.leftPenaltyStretch = (
+            frame.get('fieldLines').get('LeftPenaltyStretch').get('p1').get('x') + .5 * self.fieldLength,
+            frame.get('fieldLines').get('LeftPenaltyStretch').get('p1').get('y') + .5 * self.fieldWidth
+        )
+
+        self.rightPenaltyStretch = (
+            frame.get('fieldLines').get('RightPenaltyStretch').get('p1').get('x') + .5 * self.fieldLength,
+            frame.get('fieldLines').get('RightPenaltyStretch').get('p1').get('y') + .5 * self.fieldWidth
         )
 
         self.initialized = True
