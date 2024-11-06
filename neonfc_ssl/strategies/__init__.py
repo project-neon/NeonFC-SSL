41b6a063299b707a9e1a58974d1c4fb91117808d
gTorrez <g.ctorres09@gmail.com>
2024-11-06 14:56:24 -0300

    add(lb and rb): left and right back strategies in init

--- neonfc_ssl/strategies/__init__.py
+++ neonfc_ssl/strategies/__init__.py
@@ -3,4 +3,6 @@ from neonfc_ssl.strategies.receiver import Receiver
 from neonfc_ssl.strategies.ball_holder import BallHolder
 from neonfc_ssl.strategies.goalkeeper import GoalKeeper
 from neonfc_ssl.strategies.libero import Libero
+from neonfc_ssl.strategies.right_back import RightBack
+from neonfc_ssl.strategies.left_back import LeftBack
 from neonfc_ssl.strategies.base_strategy import BaseStrategy
