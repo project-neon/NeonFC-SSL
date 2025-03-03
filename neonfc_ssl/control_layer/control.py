8a688f71ec0b5d93924750cbae8093291b0c3227
Joao Pedro Leal <jpkleal@gmail.com>
2025-03-03 15:43:31 -0300

    feat(multiprocess): refactor control layer

---
+++ neonfc_ssl/control_layer/control.py
@@ -0,0 +1,126 @@
+import logging
+import numpy as np
+import time
+import socket
+from neonfc_ssl.core import Layer
+
+# from neonfc_ssl.entities import Field, RobotRubric
+from neonfc_ssl.tracking_layer.tracking import Tracking
+from neonfc_ssl.tracking_layer.state_controller import StateController
+# from neonfc_ssl.coach import BaseCoach
+from neonfc_ssl.commons.math import reduce_ang
+from neonfc_ssl.path_planning.drunk_walk import DrunkWalk
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from neonfc_ssl.decision_layer.decision_data import DecisionData, RobotRubric
+    from neonfc_ssl.tracking_layer.tracking_data import MatchData
+
+
+class Control(Layer):
+    def __init__(self, config, log_q, event_pipe) -> None:
+        super().__init__("ControlLayer", config, log_q, event_pipe)
+
+        self.KP = 1.5
+        self.KP_ang = 2
+
+    def _start(self):
+        self.log(logging.INFO, "Starting control module starting ...")
+
+        self.logger.info("Control module started!")
+
+    def _step(self, data: 'DecisionData'):
+        for command in data.commands:
+            if command.target_pose is None:
+                continue
+            self.run_single_robot(data.world_model, command)
+
+    def run_single_robot(self, data: 'MatchData', command: 'RobotRubric'):
+        robot = data.robots[command.id]
+        field = data.field
+
+        path_planning = DrunkWalk()
+        path_planning.start((robot.x, robot.y), command.target_pose[:2])
+
+        post_thickness = 0.02
+        goal_depht = 0.18
+        goal_height = 1
+        r = 0.09
+        L = 12
+        m = 0
+
+        # -- Friendly Goalkeeper Area -- #
+        if command.avoid_area:
+            path_planning.add_static_obstacle(
+                (0, field.field_width/2 - field.penalty_width/2),
+                field.penalty_depth,
+                field.penalty_width
+            )
+        # -- Friendly Goal Posts -- #
+        path_planning.add_static_obstacle(
+            (-r-goal_depht-post_thickness, field.field_width/2 - r - goal_height/2),
+            2*r + post_thickness + goal_depht,
+            2*r + goal_height
+        )
+        # -- Opponent Goalkeeper Area -- #
+        path_planning.add_static_obstacle(
+            (field.field_length - field.penalty_depth,
+             field.field_width / 2 - field.penalty_width / 2),
+            field.penalty_depth,
+            field.penalty_width
+        )
+        # # -- Opponent Goal Posts -- #
+        # path_planning.add_static_obstacle(
+        #     (-1, -1),
+        #     self._field.fieldLength + 2,
+        #     0.7
+        # )
+        # -- Lower Field Limit -- #
+        path_planning.add_static_obstacle(
+            (-L - m, -L - m),
+            field.field_length + 2 * (m + L),
+            L + r
+        )
+        # -- Right Field Limit -- #
+        path_planning.add_static_obstacle(
+            (field.field_length + m - r, -m),
+            L,
+            field.penalty_width + 2 * m
+        )
+        # -- Upper Field Limit -- #
+        path_planning.add_static_obstacle(
+            (-L - m, field.field_width + m - r),
+            field.field_length + 2 * (m + L),
+            L
+        )
+        # -- Left Field Limit -- #
+        path_planning.add_static_obstacle(
+            (-L - m, -m),
+            L + r,
+            field.field_width + 2 * m
+        )
+
+        # -- Opponent Robots -- #
+        for opp in command.avoid_opponents:
+            opp = data.opposites[opp]
+            path_planning.add_dynamic_obstacle(opp, 0.2, np.array((opp.vx, opp.vy)))
+
+        # -- Friendly Robots -- #
+        for rob in command.avoid_allies:
+            if rob == command.id:
+                continue
+
+            rob = data.robots[rob]
+            path_planning.add_dynamic_obstacle(rob, 0.2, np.array((rob.vx, rob.vy)))
+
+        next_point = path_planning.find_path()
+
+        dx = next_point[0] - robot.x
+        dy = next_point[1] - robot.y
+
+        dt = reduce_ang(command.target_pose[2] - robot.theta)
+
+        command.move_speed = (dx * self.KP, dy * self.KP, dt * self.KP_ang)
+
+        # if self._game_state.is_stopped():
+        #     command.limit_speed(1.5)
