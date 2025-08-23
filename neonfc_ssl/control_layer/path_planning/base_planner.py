50aa79d397787e68b2c6f3cf77a779dcf4c48afe
Eduardo Makita <makitayukio1@gmail.com>
2025-08-23 16:46:40 -0300

    feat(planner): add path planner base class

---
+++ neonfc_ssl/control_layer/path_planning/base_planner.py
@@ -0,0 +1,54 @@
+from abc import ABC, abstractmethod
+from typing import List, Tuple, Optional, Union
+import numpy as np
+
+
+class BasePathPlanner(ABC):
+    def __init__(self):
+        self.start = None
+        self.goal = None
+        self.obstacles = []
+        self.map_area = None
+        self.path = None
+
+    @abstractmethod
+    def set_start(self, start: Tuple[float, float]):
+        """Set the start position"""
+        pass
+
+    @abstractmethod
+    def set_goal(self, goal: Tuple[float, float]):
+        """Set the goal position"""
+        pass
+
+    @abstractmethod
+    def set_obstacles(self, obstacles: List):
+        """Set the obstacles in the environment"""
+        pass
+
+    @abstractmethod
+    def set_map_area(self, map_area: Tuple[float, float]):
+        """Set the map boundaries"""
+        pass
+
+    @abstractmethod
+    def plan(self, *args, **kwargs) -> Union[List[Tuple[float, float]], np.ndarray]:
+        """Generate a path from start to goal"""
+        pass
+
+    @abstractmethod
+    def update(self, current_state, *args, **kwargs):
+        """Update the planner with new information (for reactive planners)"""
+        pass
+
+    def get_path(self) -> Optional[Union[List[Tuple[float, float]], np.ndarray]]:
+        """Return the computed path"""
+        return self.path
+
+    def clear(self):
+        """Reset the planner state"""
+        self.start = None
+        self.goal = None
+        self.obstacles = []
+        self.map_area = None
+        self.path = None
