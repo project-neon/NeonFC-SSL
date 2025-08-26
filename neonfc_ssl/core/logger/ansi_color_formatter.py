d1434b1b666a024d6bbfff027e056ed301a4941a
jpkleal <jpkleal@gmail.com>
2025-08-26 17:41:56 -0300

    feat(log): add colorful logs

---
+++ neonfc_ssl/core/logger/ansi_color_formatter.py
@@ -0,0 +1,19 @@
+import logging
+
+class AnsiColorFormatter(logging.Formatter):
+    def format(self, record: logging.LogRecord):
+        no_style = '\033[0m'
+        bold = '\033[91m'
+        grey = '\033[90m'
+        yellow = '\033[93m'
+        red = '\033[31m'
+        red_light = '\033[91m'
+        start_style = {
+            'DEBUG': grey,
+            'INFO': no_style,
+            'WARNING': yellow,
+            'ERROR': red,
+            'CRITICAL': red_light + bold,
+        }.get(record.levelname, no_style)
+        end_style = no_style
+        return f'{start_style}{super().format(record)}{end_style}'
