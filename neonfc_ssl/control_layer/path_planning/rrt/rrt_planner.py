20596f3361841faa65e456ae88b55a501eec5627
Eduardo Makita <makitayukio1@gmail.com>
2025-08-29 20:06:50 -0300

    fix(rrt): make rrt* planner inherit from rrt planner

--- neonfc_ssl/control_layer/path_planning/rrt/rrt_planner.py
+++ neonfc_ssl/control_layer/path_planning/rrt/rrt_planner.py
@@ -9,7 +9,7 @@ class RRTPlanner(BasePathPlanner):
         self.step_size = step_size
         self.max_iter = max_iter
         self.collision_margin = collision_margin
-        self.obstacles = []  # Store obstacles as Node objects
+        self.obstacles = []
 
     def set_start(self, start: Tuple[float, float]):
         self.start = start
@@ -72,29 +72,9 @@ class RRTPlanner(BasePathPlanner):
         return obstacles
 
 
-class RRTStarPlanner(BasePathPlanner):
+class RRTStarPlanner(RRTPlanner):
     def __init__(self, step_size=0.1, max_iter=5000, collision_margin=0.18):
-        super().__init__()
-        self.step_size = step_size
-        self.max_iter = max_iter
-        self.collision_margin = collision_margin
-        self.obstacles = []  # Store obstacles as Node objects
-
-    def set_start(self, start: Tuple[float, float]):
-        self.start = start
-
-    def set_goal(self, goal: Tuple[float, float]):
-        self.goal = goal
-
-    def set_speed(self, speed: Tuple[float, float]):
-        self.speed = speed
-
-    def set_obstacles(self, obstacles: List):
-        # Convert obstacles to Node objects
-        self.obstacles = [Node(obs[0], obs[1]) for obs in obstacles]
-
-    def set_map_area(self, map_area: Tuple[float, float]):
-        self.map_area = map_area
+        super().__init__(step_size, max_iter, collision_margin)
 
     def plan(self) -> List[Tuple[float, float]]:
         rrt_star = RRTStar(
@@ -112,29 +92,3 @@ class RRTStarPlanner(BasePathPlanner):
                                                  self.start[1] + self.step_size * self.speed[1]]]
 
         return self.path
-
-    def update(self, current_state, *args, **kwargs):
-        # RRT* is a global planner, so update is not needed
-        pass
-
-    def get_path(self):
-        return self.path
-
-    @staticmethod
-    def create_rectangle_obstacles(center, width, height, density=0.1):
-        """Convert rectangular area into discrete obstacle points"""
-        obstacles = []
-        x_start = center[0] - width / 2
-        x_end = center[0] + width / 2
-        y_start = center[1] - height / 2
-        y_end = center[1] + height / 2
-
-        x = x_start
-        while x <= x_end:
-            y = y_start
-            while y <= y_end:
-                obstacles.append((x, y))
-                y += density
-            x += density
-
-        return obstacles
