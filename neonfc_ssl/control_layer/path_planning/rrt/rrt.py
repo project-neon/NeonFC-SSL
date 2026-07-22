6b9651411632d2b322b0b5062a8104a10263a278
Álvaro José <josealvaro3421@gmail.com>
2026-07-22 15:10:26 -0300

    style: fix flake errors in ...\rrt\rrt.py using pep8 patterns

--- neonfc_ssl/control_layer/path_planning/rrt/rrt.py
+++ neonfc_ssl/control_layer/path_planning/rrt/rrt.py
@@ -6,15 +6,16 @@ from . import Node
 
 
 class RRT:
-    def __init__(self,
-             start: Tuple[float, float],
-             goal: Tuple[float, float],
-             obstacles: List[Node],
-             map_area=(9, 6),
-             collision_margin=0.18,
-             step_size=0.05,
-             max_iter=5000
-        ):
+    def __init__(
+        self,
+        start: Tuple[float, float],
+        goal: Tuple[float, float],
+        obstacles: List[Node],
+        map_area=(9, 6),
+        collision_margin=0.18,
+        step_size=0.05,
+        max_iter=5000
+    ):
 
         self.start = Node(*start)
         self.goal = Node(*goal)
@@ -33,7 +34,12 @@ class RRT:
         return Node(x, y)
 
     def get_nearest_node(self, node) -> Node:
-        distance_list = [ (node.x - node_i.x)**2 + (node.y - node_i.y)**2 for node_i in self.node_list ]
+        distance_list = [
+            (node.x - node_i.x) ** 2
+            + (node.y - node_i.y) ** 2
+            for node_i in self.node_list
+            ]
+
         min_index = np.argmin(distance_list)
 
         return self.node_list[min_index]
@@ -58,9 +64,17 @@ class RRT:
         C = - A*start.x - B*start.y
         div = math.sqrt(A**2 + B**2)
 
-        sec = lambda o: ( abs( A*o.x + B*o.y + C ) / div ) > self.collision_margin
+        # Original version of the sec function, unused to avoid lambda
+        # functions following pep8 recomendations.
+        # sec = lambda o: (
+        #     (abs(A*o.x + B*o.y + C) / div)
+        #     > self.collision_margin
+        #     )
 
-        return all( sec(obstacle) for obstacle in self.obstacles )
+        def sec(o):
+            return (abs(A*o.x + B*o.y + C) / div) > self.collision_margin
+
+        return all(sec(obstacle) for obstacle in self.obstacles)
 
     def generate_final_path(self) -> List[List[float]]:
         path = []
@@ -85,9 +99,15 @@ class RRT:
 
             if self.is_collision_free(nearest_node, new_node):
                 self.node_list.append(new_node)
-            else: continue
+            else:
+                continue
 
-            if d := math.sqrt((new_node.x - self.goal.x)**2 + (new_node.y - self.goal.y)**2) <= self.step_size:
+            d = math.sqrt(
+                (new_node.x - self.goal.x) ** 2
+                + (new_node.y - self.goal.y) ** 2
+                )
+
+            if d <= self.step_size:
                 if self.is_collision_free(new_node, self.goal):
                     self.goal.parent = new_node
                     self.goal.cost = new_node.cost + d
@@ -105,14 +125,25 @@ class RRTStar(RRT):
         self.search_radius = 2.0*self.step_size
 
     def find_neighbors(self, node):
-        return [n for n in self.node_list if np.linalg.norm([n.x - node.x, n.y - node.y]) <= self.search_radius]
+        return [
+            n for n in self.node_list
+            if np.linalg.norm([n.x - node.x, n.y - node.y])
+            <= self.search_radius
+            ]
 
     def choose_parent(self, neighbors, nearest, node):
-        min_cost = nearest.cost + np.linalg.norm([nearest.x - node.x, nearest.y - node.y])
+        min_cost = (
+            nearest.cost
+            + np.linalg.norm([nearest.x - node.x, nearest.y - node.y])
+            )
+
         best_node = nearest
 
         for neighbor in neighbors:
-            cost = neighbor.cost + np.linalg.norm([neighbor.x - node.x, neighbor.y - node.y])
+            cost = (
+                neighbor.cost
+                + np.linalg.norm([neighbor.x - node.x, neighbor.y - node.y])
+                )
 
             if cost < min_cost and self.is_collision_free(neighbor, node):
                 min_cost = cost
@@ -125,9 +156,15 @@ class RRTStar(RRT):
 
     def rewire(self, node, neighbors):
         for neighbor in neighbors:
-            cost = node.cost + np.linalg.norm([neighbor.x - node.x, neighbor.y - node.y])
+            cost = (
+                node.cost
+                + np.linalg.norm([neighbor.x - node.x, neighbor.y - node.y])
+                )
 
-            if cost < neighbor.cost and self.is_collision_free(neighbor, node):
+            if (
+                cost < neighbor.cost
+                and self.is_collision_free(neighbor, node)
+            ):
                 neighbor.parent = node
                 neighbor.cost = cost
 
@@ -145,9 +182,15 @@ class RRTStar(RRT):
                 self.choose_parent(neighbors, nearest_node, new_node)
                 self.rewire(new_node, neighbors)
                 self.node_list.append(new_node)
-            else: continue
+            else:
+                continue
 
-            if d := math.sqrt((new_node.x - self.goal.x)**2 + (new_node.y - self.goal.y)**2) <= self.step_size:
+            d = math.sqrt(
+                (new_node.x - self.goal.x) ** 2
+                + (new_node.y - self.goal.y) ** 2
+                )
+
+            if d <= self.step_size:
                 if self.is_collision_free(new_node, self.goal):
                     self.goal.parent = new_node
                     self.goal.cost = new_node.cost + d
