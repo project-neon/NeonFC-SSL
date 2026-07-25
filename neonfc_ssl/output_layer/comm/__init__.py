37f671244440e4e57a9a0c308510211ec00f347a
Eduardo Makita <makitayukio1@gmail.com>
2026-07-25 17:45:54 -0300

    feat: add protocols and support to sending commands to simcli

--- neonfc_ssl/output_layer/comm/__init__.py
+++ neonfc_ssl/output_layer/comm/__init__.py
@@ -1,2 +1,3 @@
 from .grsim_comm import GrComm
 from .serial_comm import SerialComm
+from .simcli_comm import SimCliComm
--- neonfc_ssl/output_layer/output_layer.py
+++ neonfc_ssl/output_layer/output_layer.py
@@ -1,5 +1,5 @@
 from neonfc_ssl.core import Layer
-from .comm import GrComm, SerialComm
+from .comm import SimCliComm, SerialComm
 
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
@@ -9,19 +9,19 @@ if TYPE_CHECKING:
 class OutputLayer(Layer):
     def __init__(self, config, log_q):
         super().__init__("OutputLayer", config, log_q)
-        self.gr_comm = GrComm(self.config, self.logger)
+        self.sim_comm = SimCliComm(self.config, self.logger)
         self.serial_comm = SerialComm(self.config, self.logger)
 
-        self.use_gr_sim = self.config["use_gr_sim"]
+        self.use_sim = self.config["use_gr_sim"]
 
     def _step(self, data: 'ControlData'):
-        if self.use_gr_sim:
-            self.gr_comm.update(data)
+        if self.use_sim:
+            self.sim_comm.update(data)
         else:
             self.serial_comm.update(data)
 
     def _start(self):
-        if self.use_gr_sim:
-            self.gr_comm.start()
+        if self.use_sim:
+            self.sim_comm.start()
         else:
             self.serial_comm.start()
